3116ff5999e9fbe894f2d674463005d97bc65987
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-04-08 12:10:53 -0300

    Creating a structure of the algorithm

---
+++ 1des.py
@@ -0,0 +1,17 @@
+"""DES implementation"""
+
+keys = []
+
+def makeKeys():
+    pass
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
+
+
+if __name__ == "__main__":
+    plain_text = 0b00000001
+    key = 0b0000000001
