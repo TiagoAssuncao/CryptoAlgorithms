17414e6a3a6ef67c258c003e8ecf8a18652bf116
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-04-16 10:40:09 -0300

    Adding make keys

--- 1des.py
+++ 1des.py
@@ -1,16 +1,27 @@
 """DES implementation"""
+numberKeys = 6
+keys = [[0 for x in range(8)] for x in range(numberKeys)]
 
-keys = [][]
+def makeLists(arg, length):
+    arglist = [(arg & (1 << i)) >> i for i in reversed(range(length))]
+    return arglist
 
 def makeKeys(key):
     key = key & 0b0011111111
-    keylist = [(key & (1 << i)) >> i for i in reversed(range(8))]
+    keyList = makeLists(key, 8)
+    for i in range(0, numberKeys):
+        subkey = [None]*8
+        subkey[0:7] = keyList[1:8]
+        subkey[7] = keyList[0]
+        keys[i] = subkey
+        keyList = subkey
 
-    subkey[0:9] = key[1:10]
-    subkey[10] = key[0]
-
-def encrypt():
-    pass
+def encrypt(plaint_text):
+    plain_text_list = makeLists(plain_text, 8)
+    rigth_text = [None]*4
+    left_text = [None]*4
+    rigth_text = plain_text_list[4:8]
+    left_text = plain_text_list[0:4]
 
 def decrypt():
     pass
@@ -19,3 +30,6 @@ def decrypt():
 if __name__ == "__main__":
     plain_text = 0b00000001
     key = 0b0000000001
+
+    makeKeys(key)
+    cryp_message = encrypt(plain_text)
