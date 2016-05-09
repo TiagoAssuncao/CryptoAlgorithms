289ebc04c903a52b0a046ac2a5ce09c57284dbdf
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-09 14:35:26 -0300

    Formating the key

--- list2-AES/pythonImplamatation/aes.py
+++ list2-AES/pythonImplamatation/aes.py
@@ -4,7 +4,8 @@
 
 
 def main():
-    pass
+    key = 0x2b7e151628aed2a6abf7158809cf4f3c
+    expanded_key = key_expansion(key)
 
 if __name__ == "__main__":
     main()
--- list2-AES/pythonImplamatation/keyexpansion.py
+++ list2-AES/pythonImplamatation/keyexpansion.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from util import sub_word, rot_word, make_xor, apply_rcon
+from util import sub_word, rot_word, make_xor, apply_rcon, int_to_bit_array,
+    adding_id_missing
 
-def init_expanded_key(key):
+def init_expanded_key(key_int):
     expanded_key = []
+    key = int_to_bit_array(key_int)
+    key = adding_id_missing(key)
 
     for i in range(0, 4):
         expanded_key[i] = [
