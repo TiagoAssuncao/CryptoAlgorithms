c9718649992ba007fd32729749b33e576c52f0fc
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-09 11:34:04 -0300

    Adding sub word method

--- list2-AES/pythonImplamatation/util.py
+++ list2-AES/pythonImplamatation/util.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sboxes
 
 Rcon = (
     0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
@@ -6,6 +7,18 @@ Rcon = (
     0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
     0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
 )
+
+def sub_word(word):
+    sub_word_apply = []
+
+    for i in range(0, 4):
+        current_byte = word[i]
+        left = array_bits_to_int(current_byte[0:4])
+        rigth = array_bits_to_int(current_byte[4:8])
+
+        sub_word_apply[i] = Sbox[left][rigth]
+
+    return sub_word_apply
 
 def rot_word(word):
     word[0], word[1], word[2], word[3] = word[1], word[2], word[3], word[0]
