d71f5c7805db2609d414099ac8090fba14424d1c
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-11 20:23:14 -0300

    Adding a structure to encryption file

--- list2-AES/pythonImplamatation/aes.py
+++ list2-AES/pythonImplamatation/aes.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """AES Implamatation."""
 from keyexpansion import key_expansion
-from sboxes import *
 
 def main():
-    key = 0x2b7e151628aed2a6abf7158809cf4f3c
     key = 0x000102030405060708090A0B0C0D0E0F
+    plain = 0x00112233445566778899AABBCCDDEEFF
     expanded_key = key_expansion(key)
 
 if __name__ == "__main__":
---
+++ list2-AES/pythonImplamatation/encryption.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Algorithm to encryption AES."""
+
+def sub_bytes():
+    pass
+
+def shift_rows():
+    pass
+
+def mix_columns():
+    pass
+
+def add_round_key():
+    pass
