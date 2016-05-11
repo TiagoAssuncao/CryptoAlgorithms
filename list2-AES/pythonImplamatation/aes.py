e0d53378243d5c5240198fc675ee5fcc3dceac28
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-11 20:37:42 -0300

    Adding method to init plain text

--- list2-AES/pythonImplamatation/aes.py
+++ list2-AES/pythonImplamatation/aes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """AES Implamatation."""
 from keyexpansion import key_expansion
-
+from encryption import encryption
 def main():
     key = 0x000102030405060708090A0B0C0D0E0F
     plain = 0x00112233445566778899AABBCCDDEEFF
     expanded_key = key_expansion(key)
+    ciphertext = encryption(plain)
 
 if __name__ == "__main__":
         main()
--- list2-AES/pythonImplamatation/encryption.py
+++ list2-AES/pythonImplamatation/encryption.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Algorithm to encryption AES."""
-
+from util import int_to_bit_array, adding_id_missing, separe_keys_in_bytes
 def sub_bytes():
     pass
 
@@ -10,5 +10,15 @@ def shift_rows():
 def mix_columns():
     pass
 
-def add_round_key():
+def add_round_plain():
     pass
+
+def init_plain_text(plain_int):
+    plain = int_to_bit_array(plain_int)
+    plain = adding_id_missing(plain, 128)
+    plain = separe_keys_in_bytes(plain)
+
+    return plain
+
+def encryption(plain_int):
+   plain = init_plain_text(plain_int)
