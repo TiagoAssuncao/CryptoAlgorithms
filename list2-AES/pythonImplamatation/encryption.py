ec35980274accebbfecd971f834be2f70a56b926
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-12 09:22:27 -0300

    Invert columns with rows in shift rows

--- list2-AES/pythonImplamatation/encryption.py
+++ list2-AES/pythonImplamatation/encryption.py
@@ -10,8 +10,17 @@ def sub_bytes(phrase):
 
 def shift_rows(shift):
      s = copy.copy(shift)
+     copy_0_3 = copy.copy(shift[0][3])
+     copy_0_2 = copy.copy(shift[0][2])
+     copy_1_3 = copy.copy(shift[1][3])
+     copy_0_1 = copy.copy(shift[0][1])
+     copy_1_2 = copy.copy(shift[1][2])
+     copy_2_3 = copy.copy(shift[2][3])
+
      s[0][1], s[0][2], s[0][3] = shift[1][1], shift[2][2], shift[3][3]
-     s[1][1], s[1][2], s[1][3] = shift[2][1], shift[3][2], shift[0][3]
+     s[1][1], s[1][2], s[1][3] = shift[2][1], shift[3][2], copy_0_3
+     s[2][1], s[2][2], s[2][3] = shift[3][1], copy_0_2, copy_1_3
+     s[3][1], s[3][2], s[3][3] = copy_0_1, copy_1_2, copy_2_3
      return s
 
 def mix_columns():
@@ -42,6 +51,6 @@ def encryption(plain_int, expanded_key):
    plain = init_plain_text(plain_int)
    initial_transformation = add_round_plain(plain, expanded_key[0:4])
    sub_byte = sub_bytes(initial_transformation)
+   print(sub_byte)
    shift = shift_rows(sub_byte)
-   print(sub_byte)
-   print(sub_byte)
+   print(shift)
