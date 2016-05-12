16fe82b80745b17d63887eae624eb1dc9f76c39b
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-11 22:08:12 -0300

    Applying shift rows

--- list2-AES/pythonImplamatation/encryption.py
+++ list2-AES/pythonImplamatation/encryption.py
@@ -2,13 +2,17 @@
 """Algorithm to encryption AES."""
 from util import int_to_bit_array, adding_id_missing, separe_keys_in_bytes
 from util import make_phrase_xor, sub_phrase
+import copy
 
 def sub_bytes(phrase):
     phrase_sub = sub_phrase(phrase)
     return phrase_sub
 
-def shift_rows():
-    pass
+def shift_rows(shift):
+     s = copy.copy(shift)
+     s[0][1], s[0][2], s[0][3] = shift[1][1], shift[2][2], shift[3][3]
+     s[1][1], s[1][2], s[1][3] = shift[2][1], shift[3][2], shift[0][3]
+     return s
 
 def mix_columns():
     pass
@@ -38,4 +42,6 @@ def encryption(plain_int, expanded_key):
    plain = init_plain_text(plain_int)
    initial_transformation = add_round_plain(plain, expanded_key[0:4])
    sub_byte = sub_bytes(initial_transformation)
+   shift = shift_rows(sub_byte)
+   print(sub_byte)
    print(sub_byte)
