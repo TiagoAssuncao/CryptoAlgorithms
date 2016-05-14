e47590e7a2ef9a02a15ec91ab70291b22b874293
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-14 19:14:29 -0300

    Adding nonce

--- list2-AES/pythonImplamatation/cbf.py
+++ list2-AES/pythonImplamatation/cbf.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """Cipher Feedback(CFB) Implamatation."""
+import time
+
+def get_nonce():
+    current_time = time.time()
+    return current_time
 
 def cfb():
     plains_texts = []
@@ -8,6 +13,7 @@ def cfb():
     plains_texts.append(0x00112233445566778899AABBCCDDEEF6)
 
     key = 0x000102030405060708090A0B0C0D0E0F
+    nonce = get_nonce
 
     cipher_texts = cfb_encrypt()
     descrypt_texts = cfb_decryption()
