ff0d2d4e1080aeb417c1b9a1fe6ccafe0cb9d6b1
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-14 19:10:11 -0300

    Adding the plain texts and the key

--- list2-AES/pythonImplamatation/cbf.py
+++ list2-AES/pythonImplamatation/cbf.py
@@ -2,7 +2,15 @@
 """Cipher Feedback(CFB) Implamatation."""
 
 def cfb():
-    pass
+    plains_texts = []
+    plains_texts.append(0x00112233445566778899AABBCCDDEEF4)
+    plains_texts.append(0x00112233445566778899AABBCCDDEEF5)
+    plains_texts.append(0x00112233445566778899AABBCCDDEEF6)
+
+    key = 0x000102030405060708090A0B0C0D0E0F
+
+    cipher_texts = cfb_encrypt()
+    descrypt_texts = cfb_decryption()
 
 if __name__ == "__main__":
     cfb()
