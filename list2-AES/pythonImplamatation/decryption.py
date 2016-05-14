3c05d23a709875c2909c3ffe84cca7c4e788aa8f
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-14 10:51:50 -0300

    Construct inv shift rows

--- list2-AES/pythonImplamatation/decryption.py
+++ list2-AES/pythonImplamatation/decryption.py
@@ -2,27 +2,22 @@
 """Decryption AES Implamatation."""
 from util import make_phrase_xor, int_to_bit_array, adding_id_missing
 from util import separe_keys_in_bytes
+import copy
 
-def inv_shift_rows():
-    pass
+def inv_shift_rows(shift):
+     s = copy.copy(shift)
+
+     s[1][1], s[2][1], s[3][1], s[0][1] = s[0][1], s[1][1], s[2][1], s[3][1]
+     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+     s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
+
+     return s
 
 def inv_mix_columns():
     pass
 
 def inv_sub_bytes(shift):
-     s = copy.copy(shift)
-     copy_0_3 = copy.copy(shift[0][3])
-     copy_0_2 = copy.copy(shift[0][2])
-     copy_1_3 = copy.copy(shift[1][3])
-     copy_0_1 = copy.copy(shift[0][1])
-     copy_1_2 = copy.copy(shift[1][2])
-     copy_2_3 = copy.copy(shift[2][3])
-
-     s[0][1], s[0][2], s[0][3] = shift[1][1], shift[2][2], shift[3][3]
-     s[1][1], s[1][2], s[1][3] = shift[2][1], shift[3][2], copy_0_3
-     s[2][1], s[2][2], s[2][3] = shift[3][1], copy_0_2, copy_1_3
-     s[3][1], s[3][2], s[3][3] = copy_0_1, copy_1_2, copy_2_3
-     return s
+    pass
 
 def add_round_cipher(plain, key):
     applied_key = make_phrase_xor(plain, key)
@@ -46,7 +41,8 @@ def init_cipher_text(plain_int):
     return plain_final
 
 
-def decryption(text):
+def decryption(text, expanded_key):
     text = init_cipher_text(text)
-    initial_transformation = add_round_cipher(shift_text, expanded_key[0:4])
-    shift_text = inv_shift_rows(text)
+    initial_transformation = add_round_cipher(text, expanded_key[40:44])
+    shift_text = inv_shift_rows(initial_transformation)
+    print(shift_text)
