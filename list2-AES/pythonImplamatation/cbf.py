ece98946a814515b9fb20c818198f0b7b9fa7d39
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-05-15 10:35:43 -0300

    Adding cfb encryption

--- list2-AES/pythonImplamatation/cbf.py
+++ list2-AES/pythonImplamatation/cbf.py
@@ -2,6 +2,19 @@
 """Cipher Feedback(CFB) Implamatation."""
 import time
 
+def cfb_encrypt(texts, key, nonce):
+    cipher_texts = []
+    inicialization = nonce
+
+    last_text = inicialization
+    for i in range(len(texts)):
+        text = encryption(last_text, expanded_key)
+        text = texts[i] ^ text
+        cipher_texts.append(text)
+        last_text = text
+
+    return cipher_texts
+
 def get_nonce():
     current_time = time.time()
     return current_time
@@ -15,7 +28,7 @@ def cfb():
     key = 0x000102030405060708090A0B0C0D0E0F
     nonce = get_nonce
 
-    cipher_texts = cfb_encrypt()
+    cipher_texts = cfb_encrypt(plains_texts, key, nonce)
     descrypt_texts = cfb_decryption()
 
 if __name__ == "__main__":
