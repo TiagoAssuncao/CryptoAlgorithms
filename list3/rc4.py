a37af9b391b19c567b0d2a18a8780848bc90d809
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-01 21:22:21 -0300

    Adding method rc4

--- list3/rc4.py
+++ list3/rc4.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+def rc4(key, plain):
+    converted_key = convert_to_int(key)
+    initilize = ksa(key)
+    stream = prga(initilize)
+
+    return stream
 if __name__ == "__main__":
     key = "supersecretkey"
     plain = "plain"
