bb4de0ce9b892019f2842238c4ba9177b60c4c3a
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-01 21:29:05 -0300

    Adding method to convert strings to int

--- list3/rc4.py
+++ list3/rc4.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+def converted_key(key):
+    converted_key = []
+    for i in key:
+        converted_key.append(ord(i))
+
+    return converted_key
+
 def rc4(key, plain):
     converted_key = convert_to_int(key)
     initilize = ksa(key)
