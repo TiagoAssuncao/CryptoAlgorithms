157f7e2665e03f2c66c54d5bc69b08fe34a64f89
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-03 10:38:35 -0300

    Adding a method to test a number a

--- list3/miller-rabin.py
+++ list3/miller-rabin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Calcules if a number is prime from the method od Miller Rabin"""
 import sys
+import random
 
 def find_k_q(n):
     for q in range(3, sys.maxsize, 2):
@@ -13,6 +14,19 @@ def find_k_q(n):
                 return k, q
             k += 1
 
+def testing(k, q, n, a):
+    resp = ((a**q)%n)
+    if resp == 1:
+        return False
+
+    for j in range(k):
+        resp = ((a**(2*j*q))%n)
+        if resp == n - 1:
+            return False
+
+    return True
+
+
 
 
 
@@ -20,4 +34,5 @@ if __name__ == "__main__":
     n = int(input("Digite o numero n a ser verificado: "))
 
     k, q = find_k_q(n)
-    print (k, q)
+    is_composite = testing(k, q, n, 10)
+    print (is_composite)
