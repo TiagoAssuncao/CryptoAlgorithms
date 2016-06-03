37822f524e2eda3d875cd9cfb06a7c7b8e2d3a5a
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-03 09:56:29 -0300

    Adding the method to find a k and q

--- list3/miller-rabin.py
+++ list3/miller-rabin.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 """Calcules if a number is prime from the method od Miller Rabin"""
+import sys
+
+def find_k_q(n):
+    for q in range(3, sys.maxsize, 2):
+        resp = 0
+        k = 1
+
+        while resp < n:
+            resp = ((2**k) * q) + 1
+            if resp == n:
+                return k, q
+            k += 1
+
+
+
 
 if __name__ == "__main__":
-    n = int(raw_imput("Digite o numero n a ser verificado"))
+    n = int(input("Digite o numero n a ser verificado: "))
 
     k, q = find_k_q(n)
+    print (k, q)
