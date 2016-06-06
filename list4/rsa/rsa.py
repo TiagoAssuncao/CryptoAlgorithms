c46295b7644d3c9259d906a66ba9148049a97e21
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-06 12:05:14 -0300

    Testing if a number e is valid

--- list4/rsa/rsa.py
+++ list4/rsa/rsa.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+def e_is_valid(fi_n, e):
+    pass
+
 if __name__ == "__main__":
     p = int(input("Entre com o primo p"))
     q = int(input("Entre com o primo q"))
     e = int(input("Entre com a chave e"))
 
     n = p * q
+    fi_n = (p-1)*(q-1)
+    if e_is_valid(fi_n, e):
+        print("O valor de e é valido")
+    else:
+        raise ValueError("O valor de e é invalido")
