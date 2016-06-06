11bb4756a6ad7e829adfb90d9da6b1087a70f759
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-06 11:59:16 -0300

    Adding a file to resolve RSA

---
+++ list4/rsa/rsa.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    p = int(input("Entre com o primo p"))
+    q = int(input("Entre com o primo q"))
+    e = int(input("Entre com a chave e"))
+
+    n = p * q
