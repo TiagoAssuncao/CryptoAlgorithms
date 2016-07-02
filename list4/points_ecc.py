e183105a3c393ee50b1fc04ffc043bf3c71b7b60
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-07-02 16:35:24 -0300

    Adding a program to find points on ecc

---
+++ list4/points_ecc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Implementation to list points on ecc."""
+
+
+
+
+if __name__ == "__main__":
+    a = input("Digite o coeficiente a: ")
+    b = input("Digite o coeficiente b: ")
+    p = input("Digite o número primo p: ")
+
+    points_on_ecc = find_points()
