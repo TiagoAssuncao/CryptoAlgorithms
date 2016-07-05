786c174ca22537a7376fc3cebd22e1f8d9735bc9
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-07-05 10:21:11 -0300

    Fixing bug on miller rabin

--- list3/miller-rabin.py
+++ list3/miller-rabin.py
@@ -4,14 +4,15 @@ import sys
 
 
 def find_k_q(n):
-    for q in range(3, sys.maxsize, 2):
-        resp = 0
+    for q in range(1, sys.maxsize, 2):
         k = 1
+        resp = 0
 
         while resp < n:
             resp = ((2**k) * q) + 1
             if resp == n:
                 return k, q
+            print(q, k)
             k += 1
 
 
@@ -30,13 +31,15 @@ def testing(k, q, n, a):
 
 if __name__ == "__main__":
     n = int(input("Digite o numero n a ser verificado: "))
+    if (n%2 == 0) and (n != 2):
+        print("O numero é composto")
+        exit()
 
     k, q = find_k_q(n)
     is_composite = False
     for a in range(1, n):
         resp = testing(k, q, n, a)
         if resp is True:
-            print(a)
             is_composite = True
 
     if is_composite:
